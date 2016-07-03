95be8abbd54ef26f86aae88a6558179af3048b34
Celeste Glavin <cmugla@gmail.com>
2016-07-03 13:26:31 -0400

    add styles to hood page

--- hood_guide_app/routes/dataRoute.js
+++ hood_guide_app/routes/dataRoute.js
@@ -6,7 +6,9 @@ const dataRouter      = require('express').Router()
 dataRouter.get('/location', apiData.showArticle, apiData.showEvent, function(req,res){
   res.render('hood/index', { articleDocs : res.results.docs,
                                   events : res.eventResults,
-                                    user : req.session.user })
+                                    user : req.session.user,
+                                location : req.query.location,
+                                 borough : req.query.borough })
 })
 
 module.exports = dataRouter;
